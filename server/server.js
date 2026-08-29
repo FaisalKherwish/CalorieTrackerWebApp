eeeab041723801d6a9e48d35126ec3782b3d7730
FaisalKherwish <faisal.kherwish@wsu.edu>
2026-08-29 13:28:10 -0700

    I added a get and post route to the server and both work

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
 const express = require('express')
-const port = 5000
+const port = 8080
 const server = express()
 
+// We do this before we handle any post requests so that express is able to handle JSON data and turn it into JavaScript
+server.use(express.json())
 
-server.listen(port, ()=> console.log('Server Running on Port: ${port}') )
+// Just returns data. Nothing comes in.
+server.get('/', (req,res) =>
+{
+    const jsonData = {name: "Banana", calories: "105"}
+    res.json(jsonData)
+})
+
+// Client sending data to server i.e new food entry
+server.post('/api/entries', (req,res) =>
+{
+    console.log(req.body)
+    res.json({message: "Recieved Entry"})
+})
+
+// Start the server
+server.listen(port, ()=> console.log('Server Running on Port:', port) )
