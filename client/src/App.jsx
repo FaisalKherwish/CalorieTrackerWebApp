2df23b0c057eeaafedb836208b1521e2ca399e7a
FaisalKherwish <faisal.kherwish@wsu.edu>
2026-08-30 09:38:44 -0700

    Starting on my own react front end. Learning the basics.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+
+  let arrObj = [{name: "Banana",calories:23},{name: "Broccoli",calories: 43}]
 
   return (
     <>
       <h1>
-        Calorie Tracker APP!
+        Calorie Tracker 
       </h1>
+
+      <ul>
+        {arrObj.map(entry => <li>{entry.calories}</li>)}
+      </ul>
     </>
   )
 }
